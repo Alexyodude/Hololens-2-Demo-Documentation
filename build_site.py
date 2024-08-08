744c19277d6d157bfb6fcb852097f13727773a7a
Alex Park <28675400+Alexyodude@users.noreply.github.com>
2024-08-08 10:21:36 +0900

    1

--- build_site.py
+++ build_site.py
@@ -6,13 +6,15 @@ from convert_links import copy_and_convert_files
 docs_dir = 'docs'
 temp_docs_dir = 'temp_docs'
 mkdocs_temp_file = 'mkdocs_temp.yml'
+site_dir = 'site'
 
 def create_temp_mkdocs_config():
-    mkdocs_config_content = """
-site_name: My Obsidian Notes
-theme:
-  name: material
-docs_dir: temp_docs
+    mkdocs_config_content = f"""
+    site_name: My Obsidian Notes
+    theme:
+    name: material
+    docs_dir: {temp_docs_dir}
+    site_dir: {site_dir}
     """
     with open(mkdocs_temp_file, 'w', encoding='utf-8') as file:
         file.write(mkdocs_config_content)
@@ -20,19 +22,29 @@ docs_dir: temp_docs
 def build_site():
     # Copy and convert markdown files
     copy_and_convert_files(docs_dir, temp_docs_dir)
-
-    # Create temporary MkDocs config
+       # Create temporary MkDocs config
     create_temp_mkdocs_config()
 
     # Build the site using MkDocs
-    subprocess.run(['mkdocs', 'build', '--site-dir', 'site', '--config-file', mkdocs_temp_file], check=True)
+    subprocess.run(['mkdocs', 'build', '--config-file', mkdocs_temp_file], check=True)
 
+def deploy_site():
+    # Deploy the site using MkDocs
+    subprocess.run(['mkdocs', 'gh-deploy', '--config-file', mkdocs_temp_file], check=True)
+
+def clean_up():
     # Clean up temporary files and config
     if os.path.exists(temp_docs_dir):
         shutil.rmtree(temp_docs_dir)
     if os.path.exists(mkdocs_temp_file):
         os.remove(mkdocs_temp_file)
+    if os.path.exists(site_dir):
+        shutil.rmtree(site_dir)
 
 if __name__ == '__main__':
-    build_site()
-    print("Site built successfully.")
+    try:
+        build_site()
+        deploy_site()
+        print("Site built and deployed successfully.")
+    finally:
+        clean_up()
